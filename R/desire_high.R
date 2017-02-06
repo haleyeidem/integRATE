e7f95ab7864892aa744ac8ebc419dce0432f6fc4
haleyeidem <haley.eidem@gmail.com>
2017-02-06 12:30:56 -0600

    Update desire_high.R documentation

--- R/desire_high.R
+++ R/desire_high.R
@@ -1,3 +1,23 @@
+#' High values are best
+#'
+#' This function maps a numeric variable to a [0, 1] scale where high values are
+#' most desirable.
+#'
+#' @details Values in the top n percent will have the highest possible
+#' desirability and values in the botton n percent will have the lowest possible
+#' desirability. Values between will be transformed to intermediate values
+#' according to the function.
+#'
+#' @param x Vector of numeric values.
+#' @param cut1,cut2 Percentiles where the desirability function changes.
+#' @param min,max Minimum (default = 0) and maximum (default = 1) desirability
+#' scores.
+#' @param scale Controls shape of the desirability function. Larger values
+#' correspond to more steep and strict curves whereas smaller values correspond
+#' to more gradual and inclusive curves.
+#' @return A numeric vector of desirability scores.
+#' @export
+
 desire_high <- function(x, cut1, cut2, min = 0, max = 1, scale = 1){
 
   if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
