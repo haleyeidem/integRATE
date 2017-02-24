c146ca8b005271d1eaab83de2ef3e8afacec6624
Jacob Steenwyk <jacob.steenwyk@vanderbilt.edu>
2017-02-24 12:18:47 -0600

    Update core functions

--- R/integRATE_core_functions_wSwitch_JLS.R
+++ R/integRATE_core_functions_wSwitch_JLS.R
@@ -3,311 +3,117 @@
 #df[,2] <- data.frame(c(runif(150, 0, 0.001), runif(400, 0.001, 0.2), runif(250, 0.2, 0.5), runif(200, 0.5, 1)))
 #colnames(df) <- c("Log Fold Change", "Adjusted P-Value")
 
-#######################################################################
-## desire #############################################################
-#######################################################################
-
-# tiny change
-
-# cut types
-cut.type <-
-  c("numerical", "num", "percentile", "per", "none", "no")
-
-# desire types
-desire.type <-
-  c("low", "l", "high", "h", "extremes", "e")
-
-# initialize function
-desire <- function(x, cut1, cut2, cut3, cut4, min = 0, max = 1, scale = 1,
-                           cut_type = cut.type, desire_type = desire.type){
-
-  # save arguments
-  cut_type <- match.arg(cut.type)
-  desire_type <- match.arg(desire.type)
-
-  # back compatabilities for cut type
-  if(method == "num") method <- "numerical"
-  if(method == "per") method <- "percentile"
-  if(method == "no") method <- "none"
-  # back compatabilities for desire type
-  if(method == "l") method <- "low"
-  if(method == "h") method <- "high"
-  if(method == "e") method <- "extremes"
-
-  # save non-nas to nna
-  if(all(nna <- !is.na(x))) nna <- TRUE
-
-  # use x[nna] and calculate desirability of those values
-
-  # checks
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  # create vector of NAs
-  y <- rep(NA,length(x))
-
-  # mod to handle NAs!!!!!!!
-
-  # initiate switch for desire types
-  switch(desire_type,
-         low = {
-           # transform data
-           y <- ((x - cut2)/(cut1 - cut2))^scale
-           switch(cut_type,
-                  numerical = {
-                    # check
-                    if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
-                    # if less than cut1 replace with 1
-                    y[x < cut1] <- 1
-                    # if greater than cut2 replace with 0
-                    y[x > cut2] <- 0
-                  },
-                  percentile = {
-                    # check
-                    if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
-                    # if x is less than cut1 replace with 1
-                    y[x < cut1] <- 1
-                    # if x is less than cut2 replace with 0
-                    y[x > cut2] <- 0
-                  },
-                  none = {
-
-                    # create articifical cuts
-                    cut1 <- min(x)
-                    cut2 <- max(x)
-                    # if x is less than or equal to min(x) replace with 1
-                    y[x = cut1] <- 1
-                    # if x is greater than or equal to max(x) replace with 0
-                    y[x = cut2] <- 0
-                  }
-
-         # start here!!!!!!!!
-
-  )
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire low numerical end ###
-
-
-### desire low percentile begin ###
-# initialize function
-desire_low_per <- function(x, per1, per2,  min = 0, max = 1, scale = 1){
-
-  # checks
-  if(per1 >= per2) stop("per1 must be less than per2\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  # create cuts
-  cut1 <- quantile(x,per1)
-  cut2 <- quantile(x,per2)
-
-  # create vector of NAs
-  y <- rep(NA,length(x))
-
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire low percentile end ###
-
-### desire low no cuts begin ###
-# initialize function
-desire_low_no_cuts <- function(x, min = 0, max = 1, scale = 1){
-
-  # checks
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire low no cuts end ###
-
-#######################################################################
-## desire high ########################################################
-#######################################################################
-
-### desire high numerical begin ###
-desire_high_num <- function(x, cut1, cut2, min = 0, max = 1, scale = 1){
-
-  # checks
-  if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  y <- rep(NA,length(x))
-  y <- ((x - cut1)/(cut2 - cut1))^scale
-  y[x < cut1] <- 0
-  y[x > cut2] <- 1
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire high numerical end ###
-
-### desire high percentile begin ###
-desire_high_per <- function(x, per1, per2, min = 0, max = 1, scale = 1){
-
-  # checks
-  if(per1 >= per2) stop("per1 must be less than per2\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  # create cuts
-  cut1 <- quantile(x,per1)
-  cut2 <- quantile(x,per2)
-
-  y <- rep(NA,length(x))
-  y <- ((x - cut1)/(cut2 - cut1))^scale
-  y[x < cut1] <- 0
-  y[x > cut2] <- 1
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire high percentile end ###
-
-### desire high no cuts begin ###
-desire_high_per <- function(x, min = 0, max = 1, scale = 1){
-
-  # checks
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  # create articifical cuts
-  # where cut1 is the lowest value
-  # and cut2 is the highest value
-  cut1 <- min(x)
-  cut2 <- max(x)
-
-  y <- rep(NA,length(x))
-  y <- ((x - cut1)/(cut2 - cut1))^scale
-  y[x = cut1] <- 0
-  y[x = cut2] <- 1
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire high no cuts end ###
-
-#######################################################################
-## desire extremes ####################################################
-#######################################################################
-
-### desire extremes numerical start ###
-desire_extremes_num <- function(x, cut1, cut2, cut3, cut4, min = 0, max = 1,
-                                scale = 1){
-
-  if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
-  if(cut2 >= cut3) stop("cut2 must be less than cut3\n")
-  if(cut3 >= cut4) stop("cut3 must be less than cut4\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  y <- rep(NA, length(x))
-  for (i in 1:length(x)){
-    if (is.na(x[i])) next
-    if (x[i] <= cut1 | x[i] >= cut4)  y[i] <- 1
-    if (x[i] >= cut2 & x[i] <= cut3) y[i] <- 0
-    if (x[i] > cut1 & x[i] < cut2) y[i] <- ((x[i] - cut2)/(cut1 - cut2))^scale
-    if (x[i] > cut3 & x[i] < cut4) y[i] <- ((x[i] - cut3)/(cut4 - cut3))^scale
-  }
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire extremes numerical end ###
-
-### desire extremes percentile start ###
-desire_extremes_per <- function(x, per1, per2, per3, per4, min = 0, max = 1,
-                                scale = 1){
-
-  if(per1 >= per2) stop("per1 must be less than per2\n")
-  if(per2 >= per3) stop("per2 must be less than per3\n")
-  if(per3 >= per4) stop("per3 must be less than per4\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  # create cuts
-  cut1 <- quantile(x,per1)
-  cut2 <- quantile(x,per2)
-  cut3 <- quantile(x,per3)
-  cut4 <- quantile(x,per4)
-
-  y <- rep(NA, length(x))
-  for (i in 1:length(x)){
-    if (is.na(x[i])) next
-    if (x[i] <= cut1 | x[i] >= cut4)  y[i] <- 1
-    if (x[i] >= cut2 & x[i] <= cut3) y[i] <- 0
-    if (x[i] > cut1 & x[i] < cut2) y[i] <- ((x[i] - cut2)/(cut1 - cut2))^scale
-    if (x[i] > cut3 & x[i] < cut4) y[i] <- ((x[i] - cut3)/(cut4 - cut3))^scale
-  }
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire extremes percentile end ###
-
-### desire extremes no cuts start ###
-desire_extremes_no_cuts <- function(x, min = 0, max = 1,
-                                    scale = 1){
-
-  # create articifical cuts
-  # where cut1 is the lowest value
-  # and cut2 is the highest value
-  cut1 <- min(x)
-  cut4 <- max(x)
-  cut2 <- 0
-  cut3 <- 0
-
-  if(cut1 >= cut2) stop("cut1 must be less than cut2\n")
-  if(cut2 >= cut3) stop("cut2 must be less than cut3\n")
-  if(cut3 >= cut4) stop("cut3 must be less than cut4\n")
-  if(min < 0 | min > 1) stop("min must be between zero and one\n")
-  if(max < 0 | max > 1) stop("max must be between zero and one\n")
-  if(scale <= 0) stop("scale must be greater than zero\n")
-
-  y <- rep(NA, length(x))
-  for (i in 1:length(x)){
-    if (is.na(x[i])) next
-    if (x[i] <= cut1 | x[i] >= cut4)  y[i] <- 1
-    if (x[i] >= cut2 & x[i] <= cut3) y[i] <- 0
-    if (x[i] > cut1 & x[i] < cut2) y[i] <- ((x[i] - cut2)/(cut1 - cut2))^scale
-    if (x[i] > cut3 & x[i] < cut4) y[i] <- ((x[i] - cut3)/(cut4 - cut3))^scale
-  }
-
-  # rescale:  min to max
-  y <- (y * (max - min))  + min
-
-  return(y)
-}
-### desire extremes no cuts end ###
+## integRATE core desirability function ##
+
+# !!!!!!!
+# CHECK WITH HALEY ABOUT CUT2/3 BEING MEDIAN AND NOT 0 --- line 106
+# !!!!!!!
+
+desire <- function(x, cut1, cut2, cut3, cut4, min = 0, max = 1, scale = 1, cut_type = cut.type, desire_type = desire.type){ # open function
+  cut.type <- c("numerical", "num", "percentile", "per", "none", "no") # possible cut types
+  desire.type <- c("low", "l", "high", "h", "extremes", "e") # possible desire types
+
+  if(!hasArg(cut_type)) cut_type <- "none" # checks
+  if(!hasArg(desire_type)) stop("\ndesire_type should be one of the following: 'low', 'high' or 'extremes'\n\nfor more details see help page ?desire()")
+  if(!is.element(desire_type, desire.type)) stop("\ndesire_type should be one of the following: 'low', 'l', 'high', 'h', 'extremes', 'e'")
+  if(!is.element(cut_type, cut.type)) stop("\ncut_type should be one of the following: 'numerical', 'num', 'percentile', 'per', 'none', 'no'")
+  if(min < 0 | min > 1) stop("\nmin must be between zero and one\n")
+  if(max < 0 | max > 1) stop("\nmax must be between zero and one\n")
+  if(scale <= 0) stop("\nscale must be greater than zero\n")
+
+  if(missing(cut_type)) cut_type <- "none" # back compatabilities
+  if(cut_type == "none") cut_type <- "no"
+  if(cut_type == "numerical") cut_type <- "num"
+  if(cut_type == "percentile") cut_type <- "per"
+  if(desire_type == "low") desire_type <- "l"
+  if(desire_type == "high") desire_type <- "h"
+  if(desire_type == "extremes") desire_type <- "e"
+
+  y <- rep(NA,length(x)) # vector of NAs
+  if(all(nna <- !is.na(x))) nna <- TRUE # True if !NA
+
+  switch(desire_type, # open desire type switch
+         l = { # open low
+           switch(cut_type, # switch for cut types within low
+                  num = { # open numerical
+                    if(cut1 >= cut2) stop("\ncut1 must be less than cut2\n") # check
+                    y <- ((x - cut2)/(cut1 - cut2))^scale # transform data
+                    y[x[nna] < cut1] <- 1
+                    y[x[nna] > cut2] <- 0
+                  }, # close numerical
+                  per = { # open percentile
+                    if(cut1 >= cut2) stop("\ncut1 must be less than cut2\n") # check
+                    per1 <- quantile(x[nna],cut1); per2 <- quantile(x[nna],cut2) # create percentile cuts
+                    y <- ((x - per2)/(per1 - per2))^scale # transform data
+                    y[x[nna] < per1] <- 1
+                    y[x[nna] > per2] <- 0
+                  }, # close percentile
+                  no = { # open none
+                    y <- ((x - cut2)/(cut1 - cut2))^scale # transform data
+                    y[x[nna] == min(x[nna])] <- 1
+                    y[x[nna] == max(x[nna])] <- 0
+                  } # close none
+                  ) # close nested cut_type switch
+         }, # close low
+         h = { # open high
+           switch(cut_type, # switch for cut types within high
+                  num = { # open numerical
+                    if(cut1 >= cut2) stop("\ncut1 must be less than cut2\n") # check
+                    y <- ((x - cut1)/(cut2 - cut1))^scale # transform data
+                    y[x[nna] < cut1] <- 0
+                    y[x[nna] > cut2] <- 1
+                  }, # close numerical
+                  per = { # open percentile
+                    per1 <- quantile(x[nna],cut1); per2 <- quantile(x[nna],cut2) # create percentile cuts
+                    y <- ((x - cut1)/(cut2 - cut1))^scale # transform data
+                    y[x[nna] < per1] <- 0
+                    y[x[nna] > per2] <- 1
+                  }, # close percentile
+                  no = { # open none
+                    y <- ((x - cut1)/(cut2 - cut1))^scale # transform data
+                    y[x[nna] == min(x[nna])] <- 0
+                    y[x[nna] == max(x[nna])] <- 1
+                  } # close none
+                  ) # end nested cut_type switch
+         }, # close high
+         e = { # open extremes
+           switch(cut_type, # switch for cut types within extremes
+                  num = { # open numerical
+                    if(cut2 >= cut3) stop("\ncut2 must be less than cut3\n") # check
+                    if(cut3 >= cut4) stop("\ncut3 must be less than cut4\n")
+                    for (i in 1:length(x)){ # open for loop
+                      if (is.na(x[i])) next # skip NA
+                      if (x[i] <= cut1 | x[i] >= cut4)  y[i] <- 1
+                      if (x[i] >= cut2 & x[i] <= cut3) y[i] <- 0
+                      if (x[i] > cut1 & x[i] < cut2) y[i] <- ((x[i] - cut2)/(cut1 - cut2))^scale
+                      if (x[i] > cut3 & x[i] < cut4) y[i] <- ((x[i] - cut3)/(cut4 - cut3))^scale
+                    } # close for loop
+                  }, # close numerical
+                  per = { # open percentile
+                    if(cut2 >= cut3) stop("\ncut2 must be less than cut3\n") # check
+                    if(cut3 >= cut4) stop("\ncut3 must be less than cut4\n")
+                    # create cuts
+                    per1 <- quantile(x[nna],cut1); per2 <- quantile(x[nna],cut2); per3 <- quantile(x[nna],cut3); per4 <- quantile(x[nna],cut4) # create cuts
+                    for (i in 1:length(x)){ # open for loop
+                      if (is.na(x[i])) next # skip NA
+                      if (x[i] <= per1 | x[i] >= per4)  y[i] <- 1
+                      if (x[i] >= per2 & x[i] <= per3) y[i] <- 0
+                      if (x[i] > per1 & x[i] < per2) y[i] <- ((x[i] - per2)/(per1 - per2))^scale
+                      if (x[i] > per3 & x[i] < per4) y[i] <- ((x[i] - per3)/(per4 - per3))^scale
+                    } # close for loop
+                  }, # close percentile
+                  no = { # open none
+                    cut1 <- min(x[nna]); cut4 <- max(x[nna]); cut2 <- 0; cut3 <- 0 # create cuts # CHECK WITH HALEY ABOUT CUT2/3 BEING MEDIAN AND NOT 0
+                    for (i in 1:length(x)){ # open for loop
+                      if (is.na(x[i])) next # skip NA
+                      if (x[i] <= cut1 | x[i] >= cut4)  y[i] <- 1
+                      if (x[i] >= cut2 & x[i] <= cut3) y[i] <- 0
+                      if (x[i] > cut1 & x[i] < cut2) y[i] <- ((x[i] - cut2)/(cut1 - cut2))^scale
+                      if (x[i] > cut3 & x[i] < cut4) y[i] <- ((x[i] - cut3)/(cut4 - cut3))^scale
+                    } # close for loop
+                  } # close none
+                  ) # end nested cut_type switch
+         } # close extremes
+    ) # close desire_type switch
+  y <- (y * (max - min)) + min; return(y)# rescale according min to max and return
+} # close function
